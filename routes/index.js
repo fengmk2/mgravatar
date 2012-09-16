e350ce0e802532b6ce4620a91b18de9bbb800906
jiandong <jiandong.t@gmail.com>
2012-09-16 12:00:24 +0800

    add site implement

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+exports.developerResources = function(req, res){
+  res.render('developer-resources', { title: 'sss' });
+};
